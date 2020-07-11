6cb3574374b19953fde25f2cc6e66b6d871d5484
hithesh <koushik.svsk@gmail.com>
2020-07-11 12:27:56 +0530

    lulo font

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "gatsby"
+import "./styles.css"
+
+
+
+const IndexPage = () => (
+  <h1 className="font">hello babe</h1>
+)
+
+export default IndexPage
