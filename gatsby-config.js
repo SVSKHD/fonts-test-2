4a4b8247897c10ab19545a7ad6cce25c55879714
hithesh <koushik.svsk@gmail.com>
2020-07-13 21:42:29 +0530

    final commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
       {
       resolve: `gatsby-source-strapi`,
        options: {
-      apiURL: `http://localhost:1337`,
+      apiURL: `https://strapisvskhd.herokuapp.com`,
       queryLimit: 10000, // Default to 100
       contentTypes: [`blogs`],
     },
